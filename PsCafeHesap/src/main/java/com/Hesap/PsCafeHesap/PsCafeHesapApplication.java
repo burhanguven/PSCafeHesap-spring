88173f98264ed36d82cf376ea50033a62213fed0
burhan.guven <burhanguven34@hotmail.com>
2020-05-02 12:13:24 +0300

    model olusturuldu

---
+++ PsCafeHesap/src/main/java/com/Hesap/PsCafeHesap/PsCafeHesapApplication.java
@@ -0,0 +1,13 @@
+package com.Hesap.PsCafeHesap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsCafeHesapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsCafeHesapApplication.class, args);
+	}
+
+}
---
+++ PsCafeHesap/src/main/java/com/Hesap/PsCafeHesap/model/base/BaseModel.java
@@ -0,0 +1,9 @@
+package com.Hesap.PsCafeHesap.model.base;
+
+import java.io.Serializable;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseModel implements Serializable {
+
+}
