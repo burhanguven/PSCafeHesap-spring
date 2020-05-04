468f0196a38c0898d878295bc03e864e55b243a5
burhan.guven <burhanguven34@hotmail.com>
2020-05-05 00:31:49 +0300

    repository olusturuldu

---
+++ PSHesap/PSHesap/src/main/java/com/Hesap/PSHesap/repository/PStableRepository.java
@@ -0,0 +1,9 @@
+package com.Hesap.PSHesap.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Hesap.PSHesap.model.PStable;
+
+public interface PStableRepository extends JpaRepository<PStable, Integer> {
+
+}
